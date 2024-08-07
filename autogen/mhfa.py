1ea6f3946d801ed95cf60839db1069e08b768ad3
LUV SINGH <luv@peopleplus.ai>
2024-08-07 18:23:09 +0530

    update

--- autogen/mhfa.py
+++ autogen/mhfa.py
@@ -4,9 +4,9 @@ from autogen.agentchat.contrib.gpt_assistant_agent import GPTAssistantAgent
 
 # Configure the agents
 config_list = autogen.config_list_from_json(
-    "OAI_CONFIG_LIST",
+    "OAI_CONFIG_LIST.json",
     filter_dict={
-        "model": ["gpt-4o-mini", "gpt-4o", "gpt-4", "gpt-4-32k-0314", "gpt-4-32k-v0314"],
+        "model": ["gpt-4o-mini", "gpt-4o", "gpt-4"], # , "gpt-4-32k-0314", "gpt-4-32k-v0314"
     },
 )
 
@@ -14,7 +14,7 @@ config_list = autogen.config_list_from_json(
 rag_agent = RetrieveUserProxyAgent(
     name="rag_agent",
     system_message="You are a retrieval agent. Your role is to fetch relevant information from the knowledge base to support the doctor's responses.",
-    human_input_mode="NEVER",
+    human_input_mode="NEVER", # ALWAYS , TERMINATE
     max_consecutive_auto_reply=3,
     retrieve_config={
         "task": "qa",
@@ -26,6 +26,30 @@ rag_agent = RetrieveUserProxyAgent(
         "get_or_create": True,
     },
 )
+
+'''
+# create the RetrieveUserProxyAgent instance named "ragproxyagent"
+corpus_file = "https://huggingface.co/datasets/thinkall/2WikiMultihopQA/resolve/main/corpus.txt"
+
+# Create a new collection for NaturalQuestions dataset
+ragproxyagent = RetrieveUserProxyAgent(
+    name="ragproxyagent",
+    human_input_mode="NEVER",
+    max_consecutive_auto_reply=3,
+    retrieve_config={
+        "task": "qa",
+        "docs_path": corpus_file,
+        "chunk_token_size": 2000,
+        "model": config_list[0]["model"],
+        "client": chromadb.PersistentClient(path="/tmp/chromadb"),
+        "collection_name": "2wikimultihopqa",
+        "chunk_mode": "one_line",
+        "embedding_model": "all-MiniLM-L6-v2",
+        "customized_prompt": PROMPT_MULTIHOP,
+        "customized_answer_prefix": "the answer is",
+    },
+)
+'''
 
 # Create a doctor-like assistant agent
 doctor_assistant = GPTAssistantAgent(

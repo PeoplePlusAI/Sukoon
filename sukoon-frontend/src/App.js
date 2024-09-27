cd2ba1def3d3652b04f5dec53b37fb0c5688c7dd
Vivek Kumar <71164425+yourresult@users.noreply.github.com>
2024-09-27 00:56:35 +0000

    Chat theme updated to new design

--- sukoon-frontend/src/App.js
+++ sukoon-frontend/src/App.js
@@ -30,7 +30,7 @@ function App() {
       });
 
       const data = await response.json();
-      setMessages(prevMessages => [...prevMessages, { text: data.answer, user: false }]);
+      setMessages(prevMessages => [...prevMessages, { text: data.full_output.agent_out.output, user: false }]);
     } catch (error) {
       console.error('Error:', error);
     } finally {
@@ -40,7 +40,7 @@ function App() {
 
   return (
     <div className="app">
-      <div className="messages-container">
+      {/* <div className="messages-container">
         {messages.map((message, index) => (
           <div key={index} className={`message ${message.user ? 'user-message' : 'bot-message'}`}>
             <p>{message.text}</p>
@@ -62,6 +62,68 @@ function App() {
           onKeyPress={(e) => e.key === 'Enter' && handleSend()}
         />
         <button onClick={handleSend}>Send</button>
+      </div> */}
+
+      <div className="chat-container" style={{ display: "flex" }}>
+        <div className="menu">
+          <div className="button" style={{ textAlign: "center" }}>
+            <i className="fa-regular fa-square-plus" style={{ fontSize: "20px" }}></i>
+            <p style={{ margin: 0 }}>New</p>
+          </div>
+          <div className="button" style={{ textAlign: "center" }}>
+            <i className="fa-solid fa-wand-magic-sparkles" style={{ fontSize: "20px" }}></i>
+            <p style={{ margin: 0 }}>Discover</p>
+          </div>
+          <div className="button" style={{ textAlign: "center" }}>
+            <i className="fa-regular fa-user" style={{ fontSize: "20px" }}></i>
+            <p style={{ margin: 0 }}>Profile</p>
+          </div>
+        </div>
+        <div className="menu-content">
+          <h4 style={{ padding: "5px" }}>Discover Chats</h4>
+          <div className="row discover">
+            <div className="col-6">
+              {/* <img src="./img/image3.png" alt="Discover 2" /> */}
+              <img src="./img/img1.png" alt="" />
+            </div>
+            <div className="col-6">
+              <img src="./img/image3.png" alt="" />
+            </div>
+            <div className="col-12">
+              <img src="./img/Frame 4.png" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="chatConv pt-4 pe-4">
+          <div className="mx-auto" style={{ width: "60%" }}>
+            <div className="messages pt-5 mt-4 mx-auto" style={{ height: "75vh" }}>
+              {messages.map((message, index) => (
+                <p key={index} className={`small p-2 me-3 my-1 rounded-3 ${message.user ? 'bg-primary text-white userMsg' : 'agentMsg'}`}>{message.text}</p>
+              ))}
+              {isLoading && (
+                <div className="loading-container">
+                  <div className="loading-bar"></div>
+                </div>
+              )}
+              {/* <p className="small p-2 me-3 my-1 text-white rounded-3 bg-primary userMsg">I need peace of mind!</p>
+              <p className="small p-2 ms-5 my-1 mt-4 rounded-3 text-dark agentMsg">That’s a great goal to have. I hope
+                this project will bring some peace of mind to the people who use it. What kind of features are
+                you planning to include? 😊</p> */}
+            </div>
+            <div className="chatAction">
+              <div className="row mx-auto query-action">
+                <div className="input-group mb-3">
+                  <div className="form-floating">
+                    <input type="text" className="w-100 h-100" id="floatingInputGroup1"
+                      placeholder="Type your message..." value={input} onChange={(e) => setInput(e.target.value)} onKeyPress={(e) => e.key === 'Enter' && handleSend()} /></div><span
+                        className="input-group-text d-flex justify-content-center submit" onClick={handleSend}><i
+                          className="fa-solid fa-arrow-up"></i>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- sukoon-frontend/src/index.js
+++ sukoon-frontend/src/index.js
@@ -19,6 +19,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
